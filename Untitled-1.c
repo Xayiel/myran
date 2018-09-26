3e018a0dc416a8df96384e36d836e4f9ca3a2fde
myran <izmyran@gmail.com>
2018-09-26 22:43:19 +0200

    xd

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,3 +1,24 @@
-#include <indpt.h>
+#include <unistd.h>
 
-int main
+int ft_putchar(char c)
+{
+write (1, &c, 1);
+return(0);
+}
+void ft_print_alphabet(void)
+{
+        int i;
+
+        i = 'a';
+        while (i <= 'z')
+        {
+            ft_putchar(i);
+            i++;
+        }
+}
+
+int main()
+{
+    ft_print_alphabet();
+    return(0);
+}
