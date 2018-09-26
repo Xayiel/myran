3d5bf4657eef03edbc5e65c0c7683dde8615ac83
myran <izmyran@gmail.com>
2018-09-26 23:01:44 +0200

    v2

--- Untitled-1.c
+++ Untitled-1.c
@@ -2,8 +2,8 @@
 
 int ft_putchar(char c)
 {
-write (1, &c, 1);
-return(0);
+write(1, &c, 1);
+return (0);
 }
 void ft_print_alphabet(void)
 {
@@ -20,5 +20,5 @@ void ft_print_alphabet(void)
 int main()
 {
     ft_print_alphabet();
-    return(0);
+    return (0);
 }
